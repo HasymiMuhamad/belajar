7c6609adc1f6c89073bfbc4c8f52f0c7d08adb8d
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-11 21:42:53 +0700

    commit

--- belajar/src/belajar.java
+++ belajar/src/belajar.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class belajar {
+
+    public int tambah (int a, int b){
+        return a+b;
+    }
+
+    public  static void main (String args[]){
+        belajar terus = new belajar();
+        int hasil = terus.tambah(12, 45);
+
+        System.out.println("Hasil Belajar = "+hasil);
+    }
+
 }
