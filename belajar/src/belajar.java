34b45f15040e44635a732db1649901c9f1f6f706
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-11 21:46:20 +0700

    commit

--- belajar/src/belajar.java
+++ belajar/src/belajar.java
@@ -4,11 +4,17 @@ public class belajar {
         return a+b;
     }
 
+    public int kurang (int a, int b){
+        return a-b;
+    }
+
     public  static void main (String args[]){
         belajar terus = new belajar();
         int hasil = terus.tambah(12, 45);
+        int hasilKurang = terus.kurang(34, 44);
 
         System.out.println("Hasil Belajar = "+hasil);
+        System.out.println("Hasil kurang "+ hasilKurang);
     }
 
 }
