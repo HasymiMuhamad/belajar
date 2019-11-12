82b482a102b35fa67f1650731ca76955bccf23b1
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-12 09:47:42 +0700

    commit

--- belajar/src/develop.java
+++ belajar/src/develop.java
@@ -42,6 +42,46 @@ public class develop {
         System.out.println(a3.perkalian());
         System.out.println(a3.pembagian());
         a3.dataPrint();
+        a3.whileLoop();
+        a3.forLoop();
+
+    }
+
+    public void whileLoop (){
+        while (a < 50){
+            System.out.println("nilai a =" + a);
+            a++;
+        }
+    }
+
+    public void forLoop (){
+        develop a5 = new develop();
+        float data1 = a5.b;
+        float data2;
+        int angka [] = {1,2,3,4,5};
+
+        for (int bilangan : angka){
+            if (bilangan  == 4){
+                break;
+            }
+
+            System.out.println("ini nilai bilangan = "+bilangan);
+        }
+
+        for (data2 = 1 ; data2 < 40 ; data2++){
+            System.out.println("nilai data2 =" + data2);
+        }
+
+        while (data1 < 40){
+            System.out.println("nilai b = "+ data1);
+            data1++;
+        }
+
+        do {
+            System.out.println("nilai A = " + data1);
+            data1++;
+        } while ( data1 < 50);
+
 
     }
 }
