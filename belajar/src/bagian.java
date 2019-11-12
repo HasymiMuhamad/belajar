d81051e6ad155c14d9c24afb8672f42db52ececd
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-12 08:14:21 +0700

    commit

--- belajar/src/bagian.java
+++ belajar/src/bagian.java
@@ -3,19 +3,21 @@ public class bagian  {
     float angka = 12;
     float angka2 = 23;
 
-    bagian() {
-        float angka;
-        float angka2;
-    }
+//    bagian() {
+//        float angka;
+//        float angka2;
+//    }
 
-    public float bagi (float a, float b){
-        return a/b;
+    public float bagi (){
+        bagian a3 = new bagian();
+        float hasilBagi = a3.angka/a3.angka2;
+        return hasilBagi;
     }
 
     public static void main (String args []){
         bagian a2 = new bagian();
         bagian a1 = new bagian();
-        float hasil = a1.bagi(12, 23);
+        float hasil = a1.bagi();
         float hasil2 = a2.angka;
         System.out.println(hasil2);
         System.out.println(hasil);
---
+++ belajar/src/develop.java
@@ -0,0 +1,38 @@
+public class develop {
+
+    float a = 12;
+    float b = 34;
+    float c = 44;
+
+    public float kurang (){
+        develop a1 = new develop();
+        float hasil = a1.a - a1.b;
+        return hasil;
+    }
+
+    public float tambah (){
+        develop a2 = new develop();
+        float hasil2 = a2.a + a2.b;
+        return hasil2;
+    }
+
+    public float perkalian(){
+        develop a4 = new develop();
+        float hasilKali = a4.a*a4.b;
+        return hasilKali;
+    }
+
+    public float pembagian(){
+        develop a5 = new develop();
+        float hasilBagi = a5.a/a5.b;
+        return hasilBagi;
+    }
+
+    public static void main (String args[]){
+        develop a3 = new develop();
+        System.out.println(a3.tambah());
+        System.out.println(a3.kurang());
+        System.out.println(a3.perkalian());
+        System.out.println(a3.pembagian());
+    }
+}
