99195eb63e93b1036d0baf19b23bf426c580c839
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-12 07:14:45 +0700

    commit

--- belajar/src/learnConstructor.java
+++ belajar/src/learnConstructor.java
@@ -11,6 +11,13 @@ class learnClass {
     public static void main (String args []){
         learnConstructor t1 = new learnConstructor();
         learnConstructor t2 = new learnConstructor();
+        String latihan = "panjang data masa depan";
+        System.out.println(latihan.length());
+        if (latihan.length() < 13 ){
+            System.out.println("Benar");
+        } else {
+            System.out.println("Salah");
+        }
         System.out.println("Hasil nya ="+ t1.num + "dan" + t2.num);
     }
 }
